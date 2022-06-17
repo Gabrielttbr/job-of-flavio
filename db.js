5f46b632352aedf7865e0e0dc664da6ff241764b
Gabrielttbr <ttbr2888@gmail.com>
2022-06-17 17:03:08 -0300

    Variáveis de ambiente configuradas

--- db.js
+++ db.js
@@ -3,11 +3,11 @@
 const mysql = require('mysql2')
 
 exports.conection =  mysql.createPool({
-    "user": 'root',
-    "password": 'root',
-    "database": 'QIKBYTE',
-    "host": 'localhost',
-    "port": '3306'
+    "user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port": process.env.MYSQL_PORT
 })
 
 exports.execute = (query, param=[]) => {
